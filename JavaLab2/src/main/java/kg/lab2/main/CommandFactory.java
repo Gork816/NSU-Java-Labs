054213165e032b5d115ab3d14e2e6d6150e4187c
Gork <grishkakochetkov@gmail.com>
2025-03-18 01:17:23 +0700

    Create CommandFactory.java

---
+++ JavaLab2/src/main/java/kg/lab2/main/CommandFactory.java
@@ -0,0 +1,30 @@
+package kg.lab2.main;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class CommandFactory {
+    private final Properties commandMappings = new Properties();
+
+    public CommandFactory() {
+        try (InputStream input = getClass().getResourceAsStream("/commands.properties")) {
+            if (input != null) {
+                commandMappings.load(input);
+            } else {
+                throw new RuntimeException("Configuration file was not found");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while loading configuration file", e);
+        }
+    }
+
+    public Command create(String commandName) throws Exception {
+        String className = commandMappings.getProperty(commandName);
+        if (className == null) {
+            throw new IllegalArgumentException("Unknown command: " + commandName);
+        }
+        return (Command) Class.forName(className).getDeclaredConstructor().newInstance();
+    }
+}
+
